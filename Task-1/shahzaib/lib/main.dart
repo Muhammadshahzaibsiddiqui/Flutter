763f9e0ca65c8d762a4163247deef09bdbeb4559
Muhammadshahzaibsiddiqui <s.zaib356@gmail.com>
2021-05-11 18:14:02 +0500

    Hello World Task

---
+++ Task-1/shahzaib/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Text("Name: Muhammad Shahzaib Siddiqui\nEmail: s.zaib356@gmail.com",style: TextStyle(fontSize: 16.0 ,fontWeight:FontWeight.bold,color: Colors.black)),
+      ),
+    );
+  }
+}
